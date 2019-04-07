9598ec53a608cda7e5747519efa93acc56adf67a
Eunbyul Cho <hello@eunbyulcho.me>
2019-04-07 23:01:50 +0900

    add formspree

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "././mail/contact_me.php",
+        url: "https://formsprese.io/hello@eunbyul.com",
         type: "POST",
         data: {
           name: name,
